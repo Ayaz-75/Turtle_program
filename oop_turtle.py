7e0e8928ae327b1ccbd473778bdcfd0213fadbac
Ayaz Ali <ayazalilakho2@gmail.com>
2021-07-11 23:29:23 +0500

    Ayaz 75

---
+++ oop_turtle.py
@@ -0,0 +1,17 @@
+from turtle import Turtle,Screen, goto
+timmy=Turtle()
+print(timmy)
+
+my_screen=Screen()
+my_screen=Screen()
+
+timmy.shape("turtle")
+timmy.color("Black")
+timmy.forward(100)
+timmy.fd(10)
+timmy.circle(50)
+
+print(my_screen.canvheight)
+
+my_screen.bgcolor("White")
+my_screen.exitonclick()
